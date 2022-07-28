855a50b61cb43624912dc46fdd1c4fa393e3a83a
Madaraka02 <victormadaraka@gmail.com>
2022-07-28 18:22:26 +0300

    edited cartpage

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -33,7 +33,7 @@ function Cart() {
     <tr>
       <th scope="col"><p className="text-sm">Item</p></th>
       <th scope="col"><p className="text-sm">Quantity</p></th>
-      <th scope="col"></th>
+      <th scope="col">Action</th>
       <th scope="col"></th>
       <th scope="col"></th>
     </tr>
@@ -48,21 +48,19 @@ function Cart() {
         <p className="text-sm">{item.quantity}</p>
       </td>
       <td>
-      <button className="btn btn-outline-primary btn-sm"
+      <button className="btn btn-outline-primary btn-sm m-2"
             onClick={() => updateItemQuantity(item.id, item.quantity + 1)}
           >
             +
           </button>
-      </td>
-      <td>
-      <button className="btn btn-outline-danger btn-sm"
+
+      <button className="btn btn-outline-danger btn-sm m-2"
             onClick={() => updateItemQuantity(item.id, item.quantity - 1)}
           >
             -
           </button>
-      </td>
-      <td>
-      <button className="btn btn-outline-danger btn-sm" onClick={() => removeItem(item.id)}>&times;</button>
+
+      <button className="btn btn-outline-danger btn-sm m-2" onClick={() => removeItem(item.id)}>&times;</button>
       </td>
     </tr>
 
